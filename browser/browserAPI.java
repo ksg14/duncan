114010d88a1a9674805099aeadd28139d1db3e0b
mohit raj <mohit.raj@st.niituniversity.in>
2017-03-28 07:23:25 +0530

    Browser Script

---
+++ browser/browserAPI.java
@@ -0,0 +1,46 @@
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class browserAPI 
+{
+	
+    public static void browserScript ()
+    {
+        String url = "http://www.google.com";
+
+        if(Desktop.isDesktopSupported())
+        {
+            
+        	Desktop desktop = Desktop.getDesktop();
+            
+        	try 
+            {
+                desktop.browse(new URI(url));
+            }
+            
+            catch (IOException | URISyntaxException e) 
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        
+        else
+        {
+            Runtime runtime = Runtime.getRuntime();
+            
+            try 
+            {
+                runtime.exec("xdg-open " + url);
+            }
+            
+            catch (IOException e) 
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ browser/driver.java
@@ -0,0 +1,8 @@
+public class driver
+{
+	public static void main(String args [])
+	{
+		browserAPI driver = new browserAPI();
+		driver.browserScript();
+	}
+}
