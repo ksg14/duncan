c32e069caee629e92398c962778716c2b8f58460
prateek-agarwal <prateek.pro95@gmail.com>
2017-04-10 15:19:06 +0530

    counting feature added

--- media/indexing.py
+++ media/indexing.py
@@ -1,7 +1,14 @@
+#Python3
+
 import os
 import subprocess, sys
-rootdir = '/home/prateek'
+
+#Rootdir for windows to figure out is remaining
+rootdir = '/home/'
 ext = ('.mp4','.avi')
+
+indexfile_name = 'songs.index'
+
 
 #opening media by passing in the whole path of song works for both Windows and Linux
 def open_file(filename):
@@ -11,12 +18,45 @@ def open_file(filename):
         opener ="open" if sys.platform == "darwin" else "xdg-open"
         subprocess.call([opener, filename])
 
-# where indexing of .mp4 and .avi files is done
-with open(".index.txt", "w") as f:        
-    for subdir, dirs, files in os.walk(rootdir):
-        for file in files:
-            if file.endswith(ext):
-                f.write(os.path.join(subdir,file)+"\n")
-f.close()
 
-open_file('/home/prateek/Avicii - The Nights.mp4')
+def retrieve_songs_with_count():
+    name_count_tuples_list = []
+    with open(indexfile_name, "r") as f:        
+        for line in f.readlines():
+            x = [i.strip() for i in line.split('/')]
+            song_name_with_count = [i.strip() for i in x[-1].split('\t')]
+            name_count_tuples_list.append((song_name_with_count[0] , song_name_with_count[1]))
+    return name_count_tuples_list
+
+# where indexing of .mp4 and .avi files is done
+def indexing():
+    with open(indexfile_name, "w") as f:        
+        for subdir, dirs, files in os.walk(rootdir):
+            for file in files:
+                if file.endswith(ext):
+                    f.write(os.path.join(subdir,file)+'\t'+ str(0)+ "\n")
+
+def path_to_song(song_pos):
+    with open(indexfile_name,'r') as f:
+        y = f.readlines()[song_pos].split('\t')
+        return y[0].strip()
+
+def search_song(song_list,name):
+    for i,(n,c) in enumerate(song_list):
+    
+
+
+
+
+indexing()
+
+print('Enter the song you want to search for?? \n')
+x = input()
+
+name_count_tuples_list = retrieve_songs_with_count()
+
+#search_song(name_count_tuples_list,x)
+
+#print(name_count_tuples_list)
+
+# open_file('/home/prateek/Avicii - The Nights.mp4')
