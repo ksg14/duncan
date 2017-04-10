670e852a7e0d7fed0b4fb998076656c489aff7b4
prateek-agarwal <prateek.pro95@gmail.com>
2017-04-10 16:58:09 +0530

    search and play feature added

--- media/indexing.py
+++ media/indexing.py
@@ -2,13 +2,17 @@
 
 import os
 import subprocess, sys
-
+import webbrowser
 #Rootdir for windows to figure out is remaining
-rootdir = '/home/'
-ext = ('.mp4','.avi')
 
+#ext = ('.mp4','.avi','.webm','.mp3')
+ext = ('.mp3'   )
 indexfile_name = 'songs.index'
 
+if sys.platform == 'win64':
+    rootdir = 'C:\\'    
+else:
+    rootdir = '/home/'
 
 #opening media by passing in the whole path of song works for both Windows and Linux
 def open_file(filename):
@@ -16,7 +20,7 @@ def open_file(filename):
         os.startfile(filename)
     else:
         opener ="open" if sys.platform == "darwin" else "xdg-open"
-        subprocess.call([opener, filename])
+        subprocess.Popen([opener, filename])
 
 
 def retrieve_songs_with_count():
@@ -25,7 +29,7 @@ def retrieve_songs_with_count():
         for line in f.readlines():
             x = [i.strip() for i in line.split('/')]
             song_name_with_count = [i.strip() for i in x[-1].split('\t')]
-            name_count_tuples_list.append((song_name_with_count[0] , song_name_with_count[1]))
+            name_count_tuples_list.append((song_name_with_count[0].lower() , song_name_with_count[1]))
     return name_count_tuples_list
 
 # where indexing of .mp4 and .avi files is done
@@ -42,21 +46,41 @@ def path_to_song(song_pos):
         return y[0].strip()
 
 def search_song(song_list,name):
+    s_index = []
     for i,(n,c) in enumerate(song_list):
-    
+        if name in n:
+            s_index.append((i,int(c)))
+    sorted_by_count = sorted(s_index,key=lambda t:t[1] , reverse=True)
+    return sorted_by_count
+
+def count_increase(r):
+    with open(indexfile_name,'r') as f:
+        data = f.readlines()
+    #new_count = int(data[r].split('\t')[1].strip()) + 1
+    q = [i.strip() for i in data[r].split('\t')]
+    data[r] =  q[0] + '\t'+ str(int(q[1]) + 1) + '\n'
+    with open(indexfile_name,'w') as f:
+        f.writelines(data)
+
+def checking_counts(res):
+    f = res[0][1]
+    open_file(path_to_song(res[0][0]))
+    count_increase(res[0][0])
 
 
 
-
-indexing()
+#indexing()
 
 print('Enter the song you want to search for?? \n')
 x = input()
 
 name_count_tuples_list = retrieve_songs_with_count()
 
-#search_song(name_count_tuples_list,x)
-
+res = search_song(name_count_tuples_list,x)
+if len(res):
+    checking_counts(res)
+else:
+    webbrowser.open('https://www.youtube.com/results?search_query=' + x)
 #print(name_count_tuples_list)
 
 # open_file('/home/prateek/Avicii - The Nights.mp4')
