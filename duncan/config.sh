1eb15646629b4385504dfe9f3f27cf393bfad2e1
Karanjit Gill <ksg207@live.com>
2017-04-11 16:17:28 +0530

    :memo: Added usage details for config.sh
    Added details for environment vars in config.sh

--- duncan/config.sh
+++ duncan/config.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#Usage . config.sh
+#export JAVA_HOME=<jdk dir path>
+#export PATH=<apache-maven-3/bin dir path>:$PATH
+
 export JAVA_HOME=/usr/local/java/jdk1.8.0_121/
 export PATH=~/Downloads/apache-maven-3.3.9/bin:$PATH
 echo "Thy work has been done!"
