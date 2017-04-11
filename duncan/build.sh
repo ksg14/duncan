5cd74d71e61696cae2638192a6f80f6097f1b30f
Karanjit Gill <ksg207@live.com>
2017-04-11 16:19:31 +0530

    :wheelchair: Removed system path dependency
    Removed sys path dependency but script requires root permissions

--- duncan/build.sh
+++ duncan/build.sh
@@ -3,8 +3,8 @@
 package=$1
 module=$2
 
-#Create Voice directory ***System Dependent***
-mkdir -p /home/phiber/Desktop/Work/duncan/duncan/target/voice/
+#Create Voice directory
+mkdir -p /home/duncan_voice/
 
 echo "Build Started for $package.$module"
 java -cp target/duncan-1.0-SNAPSHOT.jar $package.$module
