482d58ff5321cd299c14f4b78ae9454c61dc131b
Karanjit Gill <ksg207@live.com>
2017-04-11 15:12:50 +0530

    :wrench: fixed build.sh to add voice dir, PATH is still system dependent

--- duncan/build.sh
+++ duncan/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+#Usage bash build.sh package.module
 package=$1
 module=$2
+
+#Create Voice directory ***System Dependent***
+mkdir -p /home/phiber/Desktop/Work/duncan/duncan/target/voice/
+
 echo "Build Started for $package.$module"
 java -cp target/duncan-1.0-SNAPSHOT.jar $package.$module
