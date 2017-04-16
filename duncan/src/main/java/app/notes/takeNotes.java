57aceb6d6a28997ec1b06cc149a65ecc2601b106
Karanjit Gill <ksg207@live.com>
2017-04-16 22:07:44 +0530

    :sparkles: Notes initial commit
    Notes file added

---
+++ duncan/src/main/java/app/notes/takeNotes.java
@@ -0,0 +1,33 @@
+package notes;
+
+import java.util.Date;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class takeNotes {
+  public static void writeUserNotes (String userNote) {
+    Date sysDate = new Date ();
+
+    JSONObject note = new JSONObject();
+		note.put("Note", userNote);
+		note.put("Date", ""+sysDate.getTime());
+
+		JSONArray notesArray = new JSONArray();
+		notesArray.add(note);
+
+		try (FileWriter file = new FileWriter("/home/duncan/Documents/notes.txt")) {
+			file.write(notesArray.toJSONString());
+			System.out.println("Successfully Copied JSON Object to File...");
+			System.out.println("\nJSON Array: " + notesArray);
+
+    }
+  }
+  public static void getUserNotes () {
+
+  }
+  public static void main(String [] args) {
+    writeUserNotes ("Hey");
+  }
+}
