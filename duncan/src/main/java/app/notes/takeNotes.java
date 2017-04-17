e0fe3c1e06a3cd86bfc141551d792bac4fd3c96f
Karanjit Gill <ksg207@live.com>
2017-04-17 18:57:40 +0530

    :sparkles: notes module added
    takeNotes updated

--- duncan/src/main/java/app/notes/takeNotes.java
+++ duncan/src/main/java/app/notes/takeNotes.java
@@ -31,35 +31,35 @@
 // //     writeUserNotes ("Hey");
 // //   }
 // // }
-// package notes;
-//
-// import org.json.simple.JSONArray;
-// import org.json.simple.JSONObject;
-//
-// import java.io.FileWriter;
-// import java.io.IOException;
-//
-// public class takeNotes {
-//     final static String notesFilePath = "/home/duncan/duncan_assets/test.json";
-//     public static void main(String[] args) {
-//         JSONObject obj = new JSONObject();
-//         obj.put("name", "mkyong.com");
-//         obj.put("age", new Integer(100));
-//
-//         JSONArray list = new JSONArray();
-//         list.add("msg 1");
-//         list.add("msg 2");
-//         list.add("msg 3");
-//
-//         obj.put("messages", list);
-//
-//         try (FileWriter file = new FileWriter(notesFilePath)) {
-//             file.write(obj.toJSONString());
-//             file.flush();
-//         } catch (IOException e) {
-//             e.printStackTrace();
-//         }
-//         System.out.print(obj);
-//     }
-//
-// }
+package notes;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class takeNotes {
+    final static String notesFilePath = "/home/duncan/duncan_assets/test.json";
+    public static void main(String[] args) {
+        JSONObject obj = new JSONObject();
+        obj.put("name", "mkyong.com");
+        obj.put("age", new Integer(100));
+
+        JSONArray list = new JSONArray();
+        list.add("msg 1");
+        list.add("msg 2");
+        list.add("msg 3");
+
+        obj.put("messages", list);
+
+        try (FileWriter file = new FileWriter(notesFilePath)) {
+            file.write(obj.toJSONString());
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.print(obj);
+    }
+
+}
