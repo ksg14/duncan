46a6f265b052bcf2e9ecd8e831eba936e17c4662
Karanjit Gill <ksg207@live.com>
2017-04-18 01:51:43 +0530

    :bug: Removed dependency on css resource

--- duncan/src/main/java/app/social/SocialNetwork.java
+++ duncan/src/main/java/app/social/SocialNetwork.java
@@ -20,22 +20,22 @@ import javafx.geometry.Pos;
 import javafx.scene.layout.FlowPane;
 import javafx.geometry.Insets;
 
-public class SocialNetwork extends Region 
-{    
+public class SocialNetwork extends Region
+{
     final WebView browser = new WebView();
     final WebEngine webEngine = browser.getEngine();
     public FlowPane flowPane = new FlowPane();
-     
+
     public static void showBrowser(Stage stage, HBox panel, String site)
     {
         stage.setTitle("Dunsinane Castle");
         Scene scene = new Scene(new SocialNetwork(site, panel),800,700,Color.web("#666970"));
         stage.setScene(scene);
-        scene.getStylesheets().add("webviewsample/BrowserToolbar.css");        
+        //scene.getStylesheets().add("webviewsample/BrowserToolbar.css");        
         stage.show();
-    } 
-    public SocialNetwork(String site, HBox panel) 
-    {   
+    }
+    public SocialNetwork(String site, HBox panel)
+    {
         //apply the styles
         getStyleClass().add("browser");
 
@@ -46,31 +46,31 @@ public class SocialNetwork extends Region
         panel.setPadding(new Insets(0,50,0,100));
         flowPane.getChildren ().addAll(browser, panel);
         flowPane.setVgap(25);
-        
+
         //add the web view to the scene
         getChildren().add(flowPane);
- 
+
     }
-    private Node createSpacer() 
+    private Node createSpacer()
     {
         Region spacer = new Region();
         HBox.setHgrow(spacer, Priority.ALWAYS);
         return spacer;
     }
- 
-    @Override protected void layoutChildren() 
+
+    @Override protected void layoutChildren()
     {
         double w = getWidth();
         double h = getHeight()-100;
         layoutInArea(browser,0,0,w,h,0, HPos.CENTER, VPos.CENTER);
     }
- 
-    @Override protected double computePrefWidth(double height) 
+
+    @Override protected double computePrefWidth(double height)
     {
         return 750;
     }
- 
-    @Override protected double computePrefHeight(double width) 
+
+    @Override protected double computePrefHeight(double width)
     {
         return 500;
     }
